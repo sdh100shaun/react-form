e33aae96c6e6d1edcf93d8f15af40c852c82ad29
Shaun Hare <shaun.hare@nottingham.ac.uk>
2016-12-23 15:39:53 +0000

    initial load

---
+++ Attendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Attendance extends React.Component
+{
+	render(){
+		return <h1>Component Another</h1>
+	}
+}
+
+ReactDOM.render(<Attendance />,document.getElementById('app'));

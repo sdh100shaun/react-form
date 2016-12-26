3a09c29aaa88efbd0fad2c5782d4ffb9e9d7a14b
Shaun Hare <shaun.hare@nottingham.ac.uk>
2016-12-26 10:08:16 +0000

    working calendar chooser from react-dates

--- src/AttendanceFormComponent.jsx
+++ src/AttendanceFormComponent.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DateRangePicker } from 'react-dates';
 import AttendanceFormInputComponent from './AttendanceFormInputComponent.jsx';
+import 'react-dates/lib/css/_datepicker.css';
 
 class AttendanceFormComponent extends React.Component {
    constructor(props) {
     super(props);
-    this.state = {value: props.value,item:props.item,formItems:{}};
+    this.state = {value: props.value,item:props.item,formItems:{}, focusedInput: null,
+      startDate: null,
+      endDate: null,};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.onDatesChange = this.onDatesChange.bind(this);
+    this.onFocusChange = this.onFocusChange.bind(this);
   }
+
+  onDatesChange({ startDate, endDate }) {
+    this.setState({ startDate, endDate });
+    this.state.formItems['startDate'] = startDate;
+    this.state.formItems['endDate'] = endDate;
+
+  }
+
+  onFocusChange(focusedInput) {
+    this.setState({ focusedInput });
+  }
+    
+  
 
   handleInput(e) {
      this.state.formItems[e.target.name] = e.target.value;
      console.log(this.state.formItems);
     
+
+  }
+
+  handleDateChange(){
 
   }
 
@@ -25,21 +49,29 @@ class AttendanceFormComponent extends React.Component {
   }
 
   render() {
+       const { focusedInput, startDate, endDate } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
+
         <label>
           Name:
-           <AttendanceFormInputComponent value='' placeholder= 'e.g uczse' name='username' handler={this.handleInput} />
+           <AttendanceFormInputComponent value='' placeholder='e.g uczse' name='username' handler={this.handleInput} />
         </label>
         <label>
           Another name:
            <AttendanceFormInputComponent value='' name='realname' handler={this.handleInput} />
         </label>
+        <br />
+        <DateRangePicker onDatesChange={this.onDatesChange}
+          onFocusChange={this.onFocusChange}
+          focusedInput={focusedInput}
+          startDate={startDate}
+          endDate={endDate} />
+        <br />
         <input type="submit" value="Submit" />
       
       </form>
     );
   }
 }
-  
 ReactDOM.render(<AttendanceFormComponent  />,document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 var path = require('path');
-var webpack = require('webpack'); 
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports={
-	entry:'./src/main.js',
-	output:{path: __dirname,filename:'./dist/bundle.js'},
-	module:{
-		loaders:[
-		{
-			test: /\.jsx?$/,
-			loader:'babel-loader',
-			exclude: /node_modules/,
-			query:{
-				presets:['es2015','react']
-			
-				   }
-			}
-		]
-	},
+module.exports = {
+    entry: './src/main.js',
+    output: { path: __dirname, filename: './dist/bundle.js' },
+    module: {
+        loaders: [{
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+           
+            { test: /\.png$/, loader: "url-loader?limit=100000" },
+            { test: /\.jpg$/, loader: "file-loader" }
+        ]
+    },
+     plugins: [
+        new ExtractTextPlugin("./dist/css/style.css")
+    ]
 };
